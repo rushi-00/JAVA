3b96bc3363671f4ff896806fa2211da1b27b3244
Rushikesh Chobhe <rushi141414@gmail.com>
2025-01-15 09:11:58 +0530

    Add files via upload

---
+++ Array_list.java
@@ -0,0 +1,20 @@
+
+
+import java.util.ArrayList;
+
+public class Array_list {
+    public static void mian(String[] args){
+
+ArrayList<String> name =new ArrayList<String>();
+
+  
+      name.add("Aniket");
+      name.add("Rushi");
+      name.add("Akash");
+
+
+      System.out.println(name);
+
+     
+    }
+}
