566304a2142a7c5dae41d598b7fc05d59a52e5fa
Rushikesh Chobhe <rushi141414@gmail.com>
2025-01-15 09:12:55 +0530

    Update Array_list.java

--- Array_list.java
+++ Array_list.java
@@ -1,20 +1,13 @@
-
-
 import java.util.ArrayList;
 
 public class Array_list {
-    public static void mian(String[] args){
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<String>();
 
-ArrayList<String> name =new ArrayList<String>();
+        name.add("Aniket");
+        name.add("Rushi");
+        name.add("Akash");
 
-  
-      name.add("Aniket");
-      name.add("Rushi");
-      name.add("Akash");
-
-
-      System.out.println(name);
-
-     
+        System.out.println(name);
     }
 }
