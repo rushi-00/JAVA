5af890f4c33f36046f37122f130ffa05b70ae77f
Rushikesh Chobhe <rushi141414@gmail.com>
2025-01-18 22:19:26 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-/* 
+ 
 public class conditions{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -93,4 +93,4 @@ public class calculator{
     }
  }
    
- */
+ 
