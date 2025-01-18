296169ab810ece6573ab296e6ec7980e139c7536
Rushikesh Chobhe <rushi141414@gmail.com>
2025-01-18 22:18:57 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+/* 
+public class conditions{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age =sc.nextInt();
+        if(age>18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Note Adult");
+        }
+
+    }
+}
+
+
+public class even {
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int number = sc.nextInt();
+        if(number % 2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
+   
+
+
+public class greaternum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b =sc.nextInt();
+        System.out.println();
+        if(a==b){
+         System.out.println("a and b is equal");
+        }
+        else{
+            if(a>b){
+                System.out.println("A is grater than b");
+            }
+            else{
+                System.out.println("b is grater tha a");
+            }
+        }
+    }
+}
+
+ 
+
+ public class switch_cases{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int botton = sc.nextInt();
+        switch(botton){
+
+            case 1: System.out.println("hello");
+            break;
+            case 2 :System.out.println("namaste");
+            break;
+            default: System.out.println("wrong input");
+        }
+
+    }
+ }
+
+
+
+public class calculator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String operations = sc.nextLine();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        switch(operations){
+
+            case "*": System.out.println(a*b);
+            break;
+            case "-": System.out.println(a-b);
+            break;
+            case "/" : System.out.println(a/b);
+            break;
+
+            default: System.out.println("wrong input");
+        }
+
+    }
+ }
+   
+ */
