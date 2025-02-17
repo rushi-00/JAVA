ba5e17460293127c86ad75fba76528f81d5014be
Rushikesh Chobhe <rushi141414@gmail.com>
2025-02-17 10:27:41 +0530

    Add files via upload

--- calculator.java
+++ calculator.java
@@ -1,96 +1,38 @@
-import java.util.*;
+import java.util.Scanner;
 
- 
-public class conditions{
-    public static void main(String[] args) {
+public class calculator {
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        int age =sc.nextInt();
-        if(age>18){
-            System.out.println("Adult");
-        }
-        else{
-            System.out.println("Note Adult");
-        }
-
-    }
-}
-
-
-public class even {
-    public static void main(String[] args)
-    {
-        Scanner sc= new Scanner(System.in);
-        int number = sc.nextInt();
-        if(number % 2==0){
-            System.out.println("Even");
-        }
-        else{
-            System.out.println("Odd");
-        }
-    }
-}
-   
-
-
-public class greaternum {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a= sc.nextInt();
-        int b =sc.nextInt();
-        System.out.println();
-        if(a==b){
-         System.out.println("a and b is equal");
-        }
-        else{
-            if(a>b){
-                System.out.println("A is grater than b");
-            }
-            else{
-                System.out.println("b is grater tha a");
-            }
-        }
-    }
-}
-
- 
-
- public class switch_cases{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int botton = sc.nextInt();
-        switch(botton){
-
-            case 1: System.out.println("hello");
-            break;
-            case 2 :System.out.println("namaste");
-            break;
-            default: System.out.println("wrong input");
-        }
-
-    }
- }
-
-
-
-public class calculator{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String operations = sc.nextLine();
         int a = sc.nextInt();
         int b = sc.nextInt();
-        switch(operations){
+        int operator = sc.nextInt();
 
-            case "*": System.out.println(a*b);
+        switch(operator){
+            case 1:System.out.println(a+b);
             break;
-            case "-": System.out.println(a-b);
+            case 2:System.out.println(a-b);
             break;
-            case "/" : System.out.println(a/b);
+            case 3:System.out.println(a*b);
             break;
+            case 4: if (b == 0) {
+                System.out.println("Invalid input");
+            }else {
+                System.out.println(a/b);
+            }
+            break;
+            case 5: if(b==0){
+                System.out.println("Invalid Division");
+                }else{
+                    System.out.println(a%b);
+                
+            }
+            break;
+            default:System.out.println("Invalid operator");
 
-            default: System.out.println("wrong input");
+
         }
+        
 
     }
- }
-   
- 
+    
+}
